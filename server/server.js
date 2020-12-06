fab04a7074becc27d2fe9acb3bb7a2fbb112d07d
Piotr Pielecki <Sunnyo@wp.pl>
2020-12-06 10:37:33 +0100

    Installed server and added new packages

---
+++ server/server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+var cors = require('cors');
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+const port = 8085;
+
+let users = {
+    admin: { password: "123456", name: "Admin", surname: "Admin" },
+    jan: { password: "jan1234", name: "Jan", surname: "Kowalski" }
+};
+
+app.post('/auth/login', cors(), (request, response) => {
+    response.header("Access-Control-Allow-Origin", "*");
+    let login = request.body.login;
+    let password = request.body.password;
+    if (!users[login]) {
+        response.json({ logged: false, message: `User not exists ${login}.` });
+        return null;
+    } else {
+        if (users[login].password != password) {
+            response.json({ logged: false, message: `Password is not valid.` });
+        } else {
+            response.json({ logged: true, message: `User login success ${login}.` });
+        }
+    }
+});
+
+app.get('/userDetails/:login', cors(), function (request, response) {
+    response.header("Access-Control-Allow-Origin", "*");
+    let login = request.params.login;
+    let data = users[login];
+    response.status(200).send(data);
+});
+
+app.get('/allUsers', cors(), function (request, response) {
+    response.header("Access-Control-Allow-Origin", "*");
+    response.status(200).send(users);
+});
+
+app.listen(port, () => console.log(`Aplikacja działa na porcie ${port}`));
